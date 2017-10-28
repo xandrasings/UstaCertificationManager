4b67fd429679b895924e2c1efbed8ee9287ee336
Xandra <xandrabest@gmail.com>
2017-10-27 21:09:05 -0500

    select data source work

--- Imports/Modules/selectDataSource.py
+++ Imports/Modules/selectDataSource.py
@@ -4,24 +4,26 @@ from .quit import *
 
 import os
 from os import listdir
-from xlrd import open_workbook
+from xlrd import *
 
 def selectDataSource(dataType):
 	dataFile = selectExcelFile(dataType)
+	dataSheet = selectExcelSheet(dataFile)
 
 
 def selectExcelFile(dataType):
 	targetDirectory = 'Resources'
 	dataFilePath = getAbsoluteFilePath(targetDirectory)
 	validOptions = getValidOptions(dataType, dataFilePath)
-	optionIndex = solicitOptionIndex(dataType, targetDirectory, validOptions)
+	optionIndex = solicitFileOptionIndex(dataType, targetDirectory, validOptions)
 	dataFilePath = extendFilePath(dataFilePath, validOptions[optionIndex])
 	dataFile = openWorkbook(dataFilePath)
 
 	return dataFile
 
 
-def printFileOptions(pathContent):
+def printFileOptions(dataType, pathContent):
+	outputPrompt('Which file would you like to use for ' + dataType + ' data?')
 	i = 1
 	for item in list(pathContent):
 		outputOption('- (' + str(i) + ') ' + item)
@@ -74,21 +76,22 @@ def filterExcelOptions(pathContent):
 	return validOptions
 
 
-def solicitOptionIndex(dataType, targetDirectory, validOptions):
-	printFileOptions(validOptions)
-	optionIndex = getFileIndex(dataType, validOptions)
+def solicitFileOptionIndex(dataType, targetDirectory, validOptions):
+	printFileOptions(dataType, validOptions)
+	optionIndex = getFileIndex(validOptions)
 	return optionIndex
 
 
-def getFileIndex(dataType, validOptions):
+def getFileIndex(validOptions):
 	fileIndex = -1
+	maxInput = len(validOptions)
 
 	while fileIndex < 0:
-		inputIndex = prompt('Which file would you like to use for ' + dataType + ' data?')
+		inputIndex = prompt()
 		try:
 			fileIndex = int(inputIndex) - 1
-			if fileIndex < 0 or fileIndex >= len(validOptions):
-				outputUserNotice('Selection should be between 1 and ' + str(len(validOptions) + 1))
+			if fileIndex < 0 or fileIndex >= maxInput:
+				outputUserNotice('Selection should be between 1 and ' + str(maxInput))
 				fileIndex = -1
 		except:
 			if inputIndex == 'Q':
@@ -99,8 +102,55 @@ def getFileIndex(dataType, validOptions):
 
 
 def openWorkbook(filePath):
+	dataFile = ''
 	try:
-		open_workbook(filePath)
-		outputUserNotice('Opening workbook from ' + filePath)
+		dataFile = open_workbook(filePath)
+		output('Opening workbook from ' + filePath + '.')
 	except:
 		fatalQuit('Could not open workbook')
+
+	return dataFile
+
+
+def selectExcelSheet(dataFile):
+	sheetIndex = dataFile.nsheets - 1
+	if sheetIndex != 0:
+		sheetOptions = dataFile.sheet_names()
+		sheetIndex = solicitSheetOptionIndex(sheetOptions)
+
+	return sheetIndex
+
+
+def solicitSheetOptionIndex(validOptions):
+	printSheetOptions(validOptions)
+	optionIndex = getSheetIndex(validOptions)
+	output('Using data from sheet \'' + validOptions[optionIndex] + '\'.')
+	return optionIndex
+
+
+def printSheetOptions(validOptions):
+	outputPrompt('Which sheet would you like to use?')
+	i = 1
+	for item in list(validOptions):
+		outputOption('- (' + str(i) + ') ' + item)
+		i = i + 1
+	outputOption('- (Q)uit')
+
+
+def getSheetIndex(validOptions):
+	sheetIndex = -1
+	maxInput = len(validOptions)
+
+	while sheetIndex < 0:
+		inputIndex = prompt()
+		try:
+			sheetIndex = int(inputIndex) - 1
+			if sheetIndex < 0 or sheetIndex >= maxInput:
+				outputUserNotice('Selection should be between 1 and ' + str(maxInput))
+				sheetIndex = -1
+		except:
+			if inputIndex == 'Q':
+				quit()
+			outputUserNotice('Selection should be an integer')
+
+	return sheetIndex
